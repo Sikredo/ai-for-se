b472f29b61173431581a1a37c6e7327d5aeca609
Laura Randl <gollumeye@outlook.de>
2024-05-05 17:31:57 +0200

    Function Context with BERT + extra linear layer for classification

--- codebert.py
+++ codebert.py
@@ -1,12 +1,10 @@
-from torch.utils.data import Dataset
 from transformers import AutoTokenizer, AutoModel
 import torch
 from DataLoader import JsonDataLoader
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-import numpy as np
+from tqdm import tqdm
 from sklearn.metrics import accuracy_score, classification_report
-from imblearn.under_sampling import RandomUnderSampler
+import copy
+import random
 
 datafiles = [
     "./data/original_method.json",
@@ -22,101 +20,275 @@ for datafile in datafiles:
     dataloader = JsonDataLoader(datafile)
     vulnerabilities.extend(dataloader.get_prepared_data())
 
-
-class InputFeatures(object):
-    """A single training/test features for a example."""
-
-    def __init__(self,
-                 context_embeddings,
-                 label,
-                 ):
-        self.context_embeddings = context_embeddings
-        self.label = label
+# TODO:
+#  #remove lines that do not contain statements from the dataset, so that less truncation is necessary
 
 
-def convert_line_to_features(java_line, tokenizer, model):
-    code_tokens = tokenizer.tokenize(java_line["code"])
-    tokens=[tokenizer.cls_token] + code_tokens + [tokenizer.sep_token]
-    tokens_ids = tokenizer.convert_tokens_to_ids(tokens)
-    context_embeddings = model(torch.tensor(tokens_ids)[None, :])[0]
-    #print("Context Embeddings")
-    #print(context_embeddings)
-    #print(context_embeddings.shape)
-    context_embeddings_with_equal_length = torch.mean(context_embeddings, dim=1)
-    #print("After applying mean to get same length:")
-    #print(context_embeddings_with_equal_length)
-    #print(context_embeddings_with_equal_length.shape)
-    return InputFeatures(context_embeddings_with_equal_length, java_line['vulnerable'])
+"""
+For each function in the training set, iterate through the lines and duplicate vulnerable lines
+to match the amount of non-vulnerable lines
+"""
+def balance_data(training_set):
+    balanced_training_set = []
+    for function in training_set:
+        non_vul_lines = []
+        vul_lines = []
+        for line in function["code"]:
+            if line["vulnerable"]:
+                vul_lines.append(line)
+            else:
+                non_vul_lines.append(line)
+
+        num_of_duplicatoins = len(non_vul_lines) - len(vul_lines)
+
+        if num_of_duplicatoins > 0:
+            new_vul_lines = copy.deepcopy(vul_lines) * (num_of_duplicatoins // len(vul_lines))
+            balanced_lines = non_vul_lines + new_vul_lines
+        else:
+            balanced_lines = non_vul_lines + vul_lines
+
+        random.shuffle(balanced_lines)
+
+        balanced_function = copy.deepcopy(function)
+        balanced_function["code"] = balanced_lines
+        balanced_training_set.append(balanced_function)
+
+    return balanced_training_set
 
 
-class TextDataset(Dataset):
-    def __init__(self, tokenizer, model):
-        self.examples = []
-        self.vul_lines = 0
-        for vulnerability in vulnerabilities: #TODO: Think if a double foreach is needed or if we can get rid of the vulnerability level
-            for java_line in vulnerability.get("vulData"):
-                if java_line["vulnerable"]:
-                    self.vul_lines += 1
-                self.examples.append(convert_line_to_features(java_line, tokenizer, model))
+################################################
+# using hard-coded example balanced trainings and test set
+# TODO: use real data from dataset
+################################################
 
-    def __len__(self):
-        return len(self.examples)
+training = [
+    {"function": "1",
+     "code": [
+         {"line": "public static void checkDirectoryTraversal(@NonNull Path parentPath, @NonNull Path pathToCheck) {",
+          "vulnerable": False},
+         {"line": "Assert.notNull(parentPath, \"Parent path must not be null\");",
+          "vulnerable": False},
+         {"line": "Assert.notNull(pathToCheck, \"Path to check must not be null\");",
+          "vulnerable": False},
+         {"line": "if (pathToCheck.startsWith(parentPath.normalize())) {",
+          "vulnerable": True},
+         {"line": "return;",
+          "vulnerable": False},
+         {"line": "}",
+          "vulnerable": False},
+         {
+             "line": "throw new ForbiddenException(\"You do not have permission to access \" + pathToCheck).setErrorData(pathToCheck);",
+             "vulnerable": False},
+         {"line": "}",
+          "vulnerable": False}
 
-    def __getitem__(self, i):
-        return self.examples[i].context_embeddings, self.examples[i].label
+     ]
+     },
+    {"function": "2",
+     "code": [
+         {"line": "public static void checkDirectoryTraversal(@NonNull Path parentPath, @NonNull Path pathToCheck) {",
+          "vulnerable": False},
+         {"line": "Assert.notNull(parentPath, \"Parent path must not be null\");",
+          "vulnerable": False},
+         {"line": "Assert.notNull(pathToCheck, \"Path to check must not be null\");",
+          "vulnerable": False},
+         {"line": "if (pathToCheck.startsWith(parentPath.normalize())) {",
+          "vulnerable": True},
+         {"line": "return;",
+          "vulnerable": False},
+         {"line": "}",
+          "vulnerable": False},
+         {
+             "line": "throw new ForbiddenException(\"You do not have permission to access \" + pathToCheck).setErrorData(pathToCheck);",
+             "vulnerable": False},
+         {"line": "}",
+          "vulnerable": False}
 
+     ]
+     }
+]
+
+test = [
+    {"function": "1",
+     "code": [
+         {"line": "public static void checkDirectoryTraversal(@NonNull Path parentPath, @NonNull Path pathToCheck) {",
+          "vulnerable": False},
+         {"line": "Assert.notNull(parentPath, \"Parent path must not be null\");",
+          "vulnerable": False},
+         {"line": "Assert.notNull(pathToCheck, \"Path to check must not be null\");",
+          "vulnerable": False},
+         {"line": "Assert.notNull(parentPath, \"Parent path must not be null\");",
+          "vulnerable": False},
+         {"line": "Assert.notNull(pathToCheck, \"Path to check must not be null\");",
+          "vulnerable": False},
+         {"line": "if (pathToCheck.startsWith(parentPath.normalize())) {",
+          "vulnerable": True},
+         {"line": "return;",
+          "vulnerable": False},
+         {"line": "}",
+          "vulnerable": False},
+         {
+             "line": "throw new ForbiddenException(\"You do not have permission to access \" + pathToCheck).setErrorData(pathToCheck);",
+             "vulnerable": False},
+         {"line": "}",
+          "vulnerable": False}
+
+     ]
+     },
+    {"function": "2",
+     "code": [
+         {"line": "public static void checkDirectoryTraversal(@NonNull Path parentPath, @NonNull Path pathToCheck) {",
+          "vulnerable": False},
+         {"line": "Assert.notNull(parentPath, \"Parent path must not be null\");",
+          "vulnerable": False},
+         {"line": "Assert.notNull(pathToCheck, \"Path to check must not be null\");",
+          "vulnerable": False},
+         {"line": "Assert.notNull(parentPath, \"Parent path must not be null\");",
+          "vulnerable": False},
+         {"line": "Assert.notNull(pathToCheck, \"Path to check must not be null\");",
+          "vulnerable": False},
+         {"line": "if (pathToCheck.startsWith(parentPath.normalize())) {",
+          "vulnerable": True},
+         {"line": "return;",
+          "vulnerable": False},
+         {"line": "}",
+          "vulnerable": False},
+         {
+             "line": "throw new ForbiddenException(\"You do not have permission to access \" + pathToCheck).setErrorData(pathToCheck);",
+             "vulnerable": False},
+         {"line": "}",
+          "vulnerable": False}
+     ]
+     }
+]
 
 tokenizer = AutoTokenizer.from_pretrained("microsoft/codebert-base")
-# BertTokenizer.from_pretrained('bert-base-uncased') ---- Could also be used
 model = AutoModel.from_pretrained("microsoft/codebert-base")
 model.to(device)
 
-print("Started Feature Engineering...")
-dataset = TextDataset(tokenizer, model)
-print(f"  Num examples(lines) = {len(dataset)}")
-print(f"    - Vulnerable lines = {dataset.vul_lines}")
-print(f"    - NonVulnerable lines = {len(dataset) - dataset.vul_lines}")
-print("Finished Feature Engineering...")
+
+"""
+Custom PyTorch Neural Network Module using the pre-trained BERT model
+-> Add extra linear layer to classify lines into vul and non-vul using output of BERT
+"""
+class BERTClassifier(torch.nn.Module):
+    def __init__(self, bert_model, num_of_classes):
+        super(BERTClassifier, self).__init__()
+        self.bert = bert_model
+        self.dropout = torch.nn.Dropout(0.1)  # drop random 10% of the data to prevent overfitting
+        self.classifier = torch.nn.Linear(self.bert.config.hidden_size, num_of_classes)  # add linear layer
+
+    def forward(self, input_ids,
+                attention_mask):  # attention mask specifies which of the tokens from input_ids should be taken into account
+        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)  # get output from BERT model
+        pooled_output = outputs.pooler_output
+        pooled_output = self.dropout(pooled_output)
+        raw_scores = self.classifier(
+            pooled_output)  # pass output from BERT to classifier to get line vulnerability predictions
+        return raw_scores
 
 
-def split_into_train_and_test(total_data):
-    X = [] # 2D array where each row is a feature vector for one line
-    y = [] # 1D array where each label belongs to one feature vector
-
-    # store feature vectors and labels in arrays
-    for i in range(len(total_data)):
-        feature_vector, label = total_data[i]
-        X.append(feature_vector.detach().numpy())
-        y.append(int(label))
-
-    X = np.array(X)
-    y = np.array(y)
-
-    # split into training and test set: 80/20 and randomly -> TODO: later use K-fold Cross Validation
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)  # change random_state for different seed
-
-    X_train = np.squeeze(X_train, axis=1)
-    X_test = np.squeeze(X_test, axis=1)
-
-    # balance the training set
-    randomUnderSampler = RandomUnderSampler(random_state=0)
-    X_train_balanced, y_train_balanced = randomUnderSampler.fit_resample(X_train, y_train)
-
-    return X_train_balanced, y_train_balanced, X_test, y_test
+classifier = BERTClassifier(model, num_of_classes=2)
+classifier.to(device)
 
 
-print("Preparing training and test set...")
-X_train_balanced, y_train_balanced, X_test, y_test = split_into_train_and_test(dataset)
+"""
+Prepare data to put it into the model:
+Tokenize code lines
+Extract labels
+Generate attention masks (to indicate whether token is an actual token or just padding)
 
-print("Training classifier...")
-classifier = RandomForestClassifier(random_state=0, n_estimators=100) #change random_state for different seed; n_estimators is amount of trees
-classifier.fit(X_train_balanced, y_train_balanced)
+IMPORTANT: context embeddings are not computed, because the BERT model operates directly on the input ids!
+-> context embeddings are calculated directly in the model
+-> self attention is used to weigh the importance of each token in the context of a sequence
+"""
+def prepare_input_data(data):
+    input_ids = []
+    attention_masks = []
+    labels = []
+    for function in data:
+        code_lines = [line["line"] for line in function["code"]]
+        tokens = tokenizer(code_lines, add_special_tokens=True, return_tensors='pt', padding=True, truncation=True)
+        input_ids.append(tokens['input_ids'])
+        attention_masks.append(tokens['attention_mask'])
+        labels.extend([int(line['vulnerable']) for line in function["code"]])
+    input_ids = torch.cat(input_ids, dim=0)
+    attention_masks = torch.cat(attention_masks, dim=0)
+    labels = torch.tensor(labels)
+    return input_ids, attention_masks, labels
 
-print("Testing classifier...")
-y_pred = classifier.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
 
-print("Result Metrics:")
-print("Accuracy: ", accuracy)
-print(classification_report(y_test, y_pred, target_names=["non-vulnerable", "vulnerable"]))
+balanced_training = balance_data(training)
+train_input_ids, train_attention_masks, train_labels = prepare_input_data(balanced_training)
+test_input_ids, test_attention_masks, test_labels = prepare_input_data(test)
 
+# optimizer for model parameters by computing gradient descent
+optimizer = torch.optim.AdamW(classifier.parameters(), lr=2e-5)  # lr=learning rate -> TODO: tune!
+
+# loss function for getting probability from raw output (softmax function)
+loss_fn = torch.nn.CrossEntropyLoss()
+
+
+"""
+Training the Model:
+-> One epoch is an iteration where the whole training set is passed through the NN
+-> Multiple iterations for updating weights and biases to optimize them
+"""
+classifier.train()
+num_epochs = 10  # TODO: tune!
+for epoch in range(num_epochs):
+    print("Epoch", epoch + 1, "/", num_epochs)
+    epoch_loss = 0.0
+    correct_predictions = 0
+    total_predictions = 0
+
+    # train model by a) feeding the input ids and attention mask for each function example to the model
+    # then b) evaluate the predictions by comparing with the actual label using the cross entropy loss function
+    # then c) update model parameters accordingly
+    # -> minimize loss (=difference between predicted and actual label) by adapting parameters through backpropagation and optimization
+    for i in tqdm(range(len(train_input_ids)), desc="Training"):
+        input_ids = train_input_ids[i].unsqueeze(0).to(device)
+        attention_mask = train_attention_masks[i].unsqueeze(0).to(device)
+        label = train_labels[i].unsqueeze(0).to(device)
+
+        optimizer.zero_grad()
+        raw_scores = classifier(input_ids=input_ids, attention_mask=attention_mask)
+        loss = loss_fn(raw_scores, label)
+
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss += loss.item()
+        _, predicted_label = torch.max(raw_scores, 1)
+        correct_predictions += (predicted_label == label).item()
+        total_predictions += 1
+
+    epoch_loss /= len(train_input_ids)
+    accuracy = correct_predictions / total_predictions
+
+    print(f"Training: Loss={epoch_loss:.4f} | Accuracy={accuracy:.4f}\n")
+print("\n")
+
+"""
+Testing the Model & Evaluation:
+"""
+classifier.eval()
+all_predictions = []
+true_labels = []
+
+for i in tqdm(range(len(test_input_ids)), desc="Testing"):
+    input_ids = test_input_ids[i].unsqueeze(0).to(device)
+    attention_mask = test_attention_masks[i].unsqueeze(0).to(device)
+    label = test_labels[i].unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        raw_scores = classifier(input_ids=input_ids, attention_mask=attention_mask)
+
+    # exctract predicted and actual labels for all examples
+    _, predicted_label = torch.max(raw_scores, 1)
+    all_predictions.extend(predicted_label.cpu().numpy())
+    true_labels.append(label.item())
+
+accuracy = accuracy_score(true_labels, all_predictions)
+print("\nEvaluation: \nAccuracy=", accuracy)
+print(classification_report(true_labels, all_predictions, target_names=["non-vul", "vul"]))
