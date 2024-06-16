b11ce12ad1cae01bce0cce22e5a144b1cfc03951
Laura Randl <gollumeye@outlook.de>
2024-06-16 18:44:43 +0200

    simpler model with just one layer

--- classifier_training.py
+++ classifier_training.py
@@ -49,14 +49,13 @@ test_loader = DataLoader(test_dataset, batch_size=8)
 class SimpleVulnerabilityClassifier(torch.nn.Module):
     def __init__(self, input_dim, num_classes, dropout_prob=0.1):
         super(SimpleVulnerabilityClassifier, self).__init__()
-        self.fc1 = torch.nn.Linear(input_dim, 512)
+        self.fc = torch.nn.Linear(input_dim, num_classes)
         self.dropout = torch.nn.Dropout(dropout_prob)
-        self.fc2 = torch.nn.Linear(512, num_classes)
 
     def forward(self, x):
-        x = torch.relu(self.fc1(x))
         x = self.dropout(x)
-        return self.fc2(x)
+        return self.fc(x)
+
 
 # Initialize the classifier
 input_dim = train_embeddings.size(1)
